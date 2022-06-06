dc77f6f3305c593a9a652a007887458599bbb61d
GweirDBS <10523525@mydbs.ie>
2022-06-06 17:47:09 +0100

    fourth commit

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+return "<h1> Congratulations. Gemma! After much toil You have finally successfully deployed the sample Spring Boot Application. </h1>";
 }
 }
