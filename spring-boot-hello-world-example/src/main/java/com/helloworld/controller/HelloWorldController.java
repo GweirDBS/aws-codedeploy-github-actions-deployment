5ebb3d5428ec4b0a6128d260bbf8447ab3b53873
GweirDBS <10523525@mydbs.ie>
2022-06-06 18:38:20 +0100

    8th commit, automating deploy

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. Gemma! After much toil You have finally successfully deployed the sample Spring Boot Application. </h1><p> Next I will alter the workflow to go from a manual Git Action to automate deployment on Git Push</p> ";
+return "<h1> Congratulations. Gemma! After much toil You have finally successfully deployed the sample Spring Boot Application. </h1><p> Next I will alter the workflow to go from a manual Git Action to automate deployment on Git Push</p><p><b> Testing Automation!!</p></b> ";
 }
 }
